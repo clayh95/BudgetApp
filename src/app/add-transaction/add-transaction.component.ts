9963cb87d78b0ec8bd46c2d7dfca780a3dcaf698
TYLER\clay.harley <clay.harley@tylertech.com>
2018-12-27 01:15:18 -0600

    Split Transaction

--- src/app/add-transaction/add-transaction.component.ts
+++ src/app/add-transaction/add-transaction.component.ts
@@ -4,6 +4,8 @@ import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 import { DbService, tAction } from '../core/db.service';
 import { MAT_DATE_FORMATS } from '@angular/material/core';
 import {default as _rollupMoment, Moment} from 'moment';
+import { database } from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
 const moment = _rollupMoment
 
 export const MMDDYYYY_FORMAT = {
@@ -31,7 +33,10 @@ export interface addEditTrans {
 })
 export class AddTransactionComponent {
 
-  tmpDate:Moment[]
+  tmpDate:Moment[];
+  origTotal:number;
+  newTotal:number;
+  dummyCopy = new Array<ITransaction>();
 
   constructor(public ATsvc: DbService, 
               public dialogRef: MatDialogRef<AddTransactionComponent>,
@@ -41,6 +46,8 @@ export class AddTransactionComponent {
                 if (this.data[0].date !== undefined) {
                   this.tmpDate = [moment(this.data[0].date)];
                 }
+                this.origTotal = +data[0].amount;
+                this.newTotal = +data[0].amount;
               }
 
   Add() {
@@ -51,19 +58,36 @@ export class AddTransactionComponent {
     this.dialogRef.close();
   }
 
+  //The first transaction should be an update; any additional ones are from SPLIT so we're adding them
   Update() {
     this.data.map((tr, index) => {
       tr.date = this.tmpDate[index].format('MM/DD/YYYY')
-      this.ATsvc.AddOrUpdateTransaction(tr, tAction.update)
+      if (index == 0) {
+        this.ATsvc.AddOrUpdateTransaction(tr, tAction.update);
+      }
+      else {
+        this.ATsvc.AddOrUpdateTransaction(tr, tAction.add);
+      }
     })
     this.dialogRef.close();
   }
 
   Split() {
-    let t = <ITransaction>{date:"", description:"", amount:"", category:"", notes: ""}
+    if (this.data.length == 1) {
+      this.data[0].notes += ' [1 of 2]'
+      this.dummyCopy.push({...this.data[0]});
+    }
+    this.tmpDate.push(this.tmpDate[0]); //set the date to the orig date
+    let t = <ITransaction>{date:this.data[0].date, description:this.data[0].description, amount:"0", category:"", notes: `[${this.data.length} of ${this.data.length}]`}
     this.data.push(t);
-    //data should be a list of iTransaction. Each split adds an object. On save, you update the original and add each new one
-    //Show the total on split. Don't show a Submit Split button until the totals match
+    this.data.map((tr, index) => {tr.notes = tr.notes.replace(/\[[0-9] of [0-9]\]$/gi, `[${index+1} of ${this.data.length}]`)});
+    this.UpdateTotal();
+  }
+
+  UpdateTotal() {
+    if (this.data.length > 1) {
+      this.newTotal = this.data.map(tr => tr.amount).reduce((pv, v) => +pv + +v, 0);
+    }
   }
 
 
